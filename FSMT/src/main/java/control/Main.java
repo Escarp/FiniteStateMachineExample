1ede966c0fa93cfec8100173b3588d0f9292bd76
Escarp <emilio.scarpellino@gmail.com>
2019-01-24 11:49:17 +0100

    MOD : added some comments

--- FSMT/src/main/java/control/Main.java
+++ FSMT/src/main/java/control/Main.java
@@ -5,10 +5,12 @@ import model.State;
 
 public class Main {
 	public static void main( String[] args ) {
+		//Initialize some States first
 		State A = new State( "A" ) ;
 		State B = new State( "B" ) ;
 		State C = new State( "C" ) ;
 		
+		//Set their exits (and defaults)
 		A.setExit( "A" , A ) ;
 		A.setExit( "B" , B ) ;
 		
@@ -17,16 +19,21 @@ public class Main {
 		
 		C.setExit( "A" , A ) ;
 		
+		//Create the Machine specifying a start and an end
 		Machine machine = new Machine( A , C ) ;
 		
+		//Get some string (or any input that the Machine can handle)
+		//(possibly already divided)
 		String input = "C-B-A-A-B-C-A-B-A-C-A-B-C-D-F-A-B-S-A-C-B" ;
 		
 		for( String in : input.split( "-" ) ) {
+			//Advance the machine
 			String out = machine.advance( in ) ;
 			
 			System.out.print( "in : " + in ) ;
 			System.out.println( " , out : " + out ) ;
 			
+			//Check if the machine has found a sequence
 			if( machine.getFound() ) {
 				System.out.println( "Sequence found!" ) ;
 			}
